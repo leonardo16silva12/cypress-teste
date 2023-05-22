bba4294cccfacbe01640b08ed979d75b3194086d
Leonardo Silva <yleo545@hotmail.com>
2023-05-22 13:22:47 -0300

    X3

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,32 +1,34 @@
 
+beforeEach(() => {
+    cy.visit("https://devfinance-agilizei.netlify.app/#")
+});
 
 describe('Transações', () => {
     it('Cadastrar uma entrada', () => {
-        cy.visit("https://devfinance-agilizei.netlify.app/#")
-
-        cy.contains("+ Nova Transação").click()
-
-        cy.get('#description').type("Salario")
-
-        cy.get('#amount').type(2000)
-
-        cy.get('#date').type("2023-05-25")
-
-        cy.contains('button', 'Salvar').click()
-
+        criarTransacao("Salario 1", 2000)
+        cy.get("tbody tr td.description").should("have.text", "Salario 1")
     });
-    // it('Cadastrar uma saída', () => {
-    //     cy.visit("https://devfinance-agilizei.netlify.app/#")
 
-    //     cy.contains("+ Nova Transação").click()
+    it('Cadastrar uma saída', () => {
+        criarTransacao("Aluguel", -1150)
+        cy.get("tbody tr td.description").should("have.text", "Aluguel")
+    });
 
-    //     cy.get('#description').type("Aluguel")
+    it('Excluir transação', () => {
+        criarTransacao("Salario", 2000)
+        criarTransacao("Bônus", 1000)
+        // cy.contains(".description", "Salario").parent().find('img').click()
+        // ou
+        cy.contains(".description", "Salario").siblings().children('img').click()
 
-    //     cy.get('#amount').type(-1150)
-
-    //     cy.get('#date').type("2023-05-25")
-
-    //     cy.contains('button', 'Salvar').click()
-
-    // });
+        cy.get('tbody tr').should("have.length", 1)
+    });
 });
+
+function criarTransacao(descricao, valor) {
+    cy.contains("+ Nova Transação").click()
+    cy.get('#description').type(descricao)
+    cy.get('#amount').type(valor)
+    cy.get('#date').type("2023-05-25")
+    cy.contains('button', 'Salvar').click()
+}
