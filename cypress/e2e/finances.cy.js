4568e6a7f9edecb6627f033cbc8b79c9fe169024
Leonardo Silva <yleo545@hotmail.com>
2023-05-22 11:21:30 -0300

    X2

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -15,4 +15,18 @@ describe('Transações', () => {
         cy.contains('button', 'Salvar').click()
 
     });
+    // it('Cadastrar uma saída', () => {
+    //     cy.visit("https://devfinance-agilizei.netlify.app/#")
+
+    //     cy.contains("+ Nova Transação").click()
+
+    //     cy.get('#description').type("Aluguel")
+
+    //     cy.get('#amount').type(-1150)
+
+    //     cy.get('#date').type("2023-05-25")
+
+    //     cy.contains('button', 'Salvar').click()
+
+    // });
 });
